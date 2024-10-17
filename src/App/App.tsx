a137a954b7adf509203c2caf5957f34107fb65c0
Макс <maks@MacBook-Pro-Maks.local>
2024-10-17 14:56:25 +0300

    feat: поменял pg и добавил стилей для карусели

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<AllComponent />} />
+        <Route path="/massage" element={<AllComponent />} />
       </Routes>
       <Footer />
     </div>
