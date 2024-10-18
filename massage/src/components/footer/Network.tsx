cacca354df32c9974600cde57863ee1f7ee0c5c1
Макс <maks@MacBook-Pro-Maks.local>
2024-10-18 20:30:48 +0300

    fewfew

--- massage/src/components/footer/Network.tsx
+++ massage/src/components/footer/Network.tsx
@@ -1,26 +1,20 @@
-import { FunctionComponent, SVGProps } from 'react';
-import { ReactComponent as Sculpt } from '../../assets/img/sculpOrig.svg';
+import React from 'react';
 
 type NetworkProps = {
   title: string;
-  icon: FunctionComponent<SVGProps<SVGSVGElement>>;
+  img: string;
   link: string;
 };
 
-export const Network: React.FC<NetworkProps> = ({
-  title,
-  icon: Icon,
-  link,
-}) => {
+export const Network: React.FC<NetworkProps> = ({ title, img, link }) => {
   return (
     <a
       href={link}
       target="_blank"
-      rel="noopener noreferrer"
       className="no-underline"
     >
       <div className="iconText">
-        <Icon/>
+        <img className="telegramPhoto" src={img} alt="" />
         <p className="textHref">{title}</p>
       </div>
     </a>
