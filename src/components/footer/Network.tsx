3418bfe08945a89f166a37d2892ee0e552b5f56d
Макс <maks@MacBook-Pro-Maks.local>
2024-10-18 16:04:43 +0300

    feat: с типизацией anyэ

--- src/components/footer/Network.tsx
+++ src/components/footer/Network.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 type NetworkProps = {
   title: string;
-  img: FunctionComponent<SVGProps<SVGSVGElement>>;
+  img: any;
   link: string;
 };
 
@@ -11,7 +11,7 @@ export const Network: React.FC<NetworkProps> = ({ title, img, link }) => {
   return (
     <a href={link} target="_blank" className="no-underline">
       <div className="iconText">
-        <img className="telegramPhoto" src={String(img)} alt="" />
+        <img className="telegramPhoto" src={img} alt="" />
         <p className="textHref">{title}</p>
       </div>
     </a>
