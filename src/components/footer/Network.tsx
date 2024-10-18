46bb67e6edc59ff467fce3337e5758c82a4a75fd
Макс <maks@MacBook-Pro-Maks.local>
2024-10-18 16:33:29 +0300

    feat: test

--- src/components/footer/Network.tsx
+++ src/components/footer/Network.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent, SVGProps } from 'react';
+import { ReactComponent as Sculpt } from '../../assets/img/sculpOrig.svg';
 
 type NetworkProps = {
   title: string;
@@ -6,11 +7,20 @@ type NetworkProps = {
   link: string;
 };
 
-export const Network: React.FC<NetworkProps> = ({ title, icon: Icon, link }) => {
+export const Network: React.FC<NetworkProps> = ({
+  title,
+  icon: Icon,
+  link,
+}) => {
   return (
-    <a href={link} target="_blank" rel="noopener noreferrer" className="no-underline">
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="no-underline"
+    >
       <div className="iconText">
-        <Icon />
+        <Sculpt />
         <p className="textHref">{title}</p>
       </div>
     </a>
